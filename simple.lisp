ea347458dbe409bb650ce356e87b7823e09ea283
William Oliveira <sudowilliam@gmail.com>
2014-02-01 03:11:52 -0200

    adds simple lisp file

---
+++ simple.lisp
@@ -0,0 +1,10 @@
+;; sum definition
+
+(defun sum (x) (+ x x))
+(print (sum 10)) ;; 20
+(print (sum 40)) ;; 80
+
+;; square definition
+(defun square (x) (* x x))
+(print (square 22)) ;; 484
+(print (square 4)) ;; 16
