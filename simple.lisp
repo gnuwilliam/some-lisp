4335659645b13af84c55e61848c5cea5a578a818
William Oliveira <sudowilliam@gmail.com>
2014-02-02 02:27:31 -0200

    making use of lambda

--- simple.lisp
+++ simple.lisp
@@ -8,3 +8,8 @@
 (defun square (x) (* x x))
 (print (square 22)) ;; 484
 (print (square 4)) ;; 16
+
+;; square definition using lambda
+(setq square (lambda (x) (* x x)))
+(print (square 10)) ;; 100
+(print (square 4)) ;; 16
