fc73163ce1f98154dd6f61fa8d94a1b2f8988675
William Oliveira <sudowilliam@gmail.com>
2014-02-02 02:43:57 -0200

    conditionals

--- simple.lisp
+++ simple.lisp
@@ -13,3 +13,10 @@
 (setq square (lambda (x) (* x x)))
 (print (square 10)) ;; 100
 (print (square 4)) ;; 16
+
+;; conditional procedures
+(defun conditional(x)
+	(cond ((= x 0) 1)
+				((> x 0) 2)))
+(print (conditional 10)) ;; 2
+(print (conditional 0)) ;; 1
